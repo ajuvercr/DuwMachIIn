e0a70276b0326228bba5b574fb721930923e6ba9
ajuvercr <arthur.vercruysse@outlook.com>
2019-04-28 15:47:36 +0200

    some code

--- DuwMachIIn.ino
+++ DuwMachIIn.ino
@@ -1,9 +1,52 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Servo.h>
 
+typedef struct {
+  int left;
+  int right;
+  int enable;
+  int speed;
+} Motor;
+
+void init_motor(Motor* motor) {
+  pinMode(motor->left, OUTPUT);
+  pinMode(motor->right, OUTPUT);
+  pinMode(motor->enable, OUTPUT);
+}
+
+/// Speed max value is 255
+void turn_motor(Motor* motor, int speed, bool clockwise) {
+  analogWrite(motor->enable, 0);
+  digitalWrite(motor->left, LOW);
+  digitalWrite(motor->right, LOW);
+  if (clockwise)
+    digitalWrite(motor->right, HIGH);
+  else
+    digitalWrite(motor->left, HIGH);
+
+  analogWrite(motor->enable, speed);
+}
+
+Motor left = {8, 9, 10, 0};
+Motor right = {7, 6, 5, 0};
+
+void setup() {
+  init_motor(&left);
+  init_motor(&right);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  turn_motor(&left, 128, true);
+  turn_motor(&right, 128, false);
+  delay(500);
+  turn_motor(&left, 0, true);
+  turn_motor(&right, 0, true);
+  delay(50);
 
+  turn_motor(&left, 128, false);
+  turn_motor(&right, 128, true);
+  delay(500);
+  turn_motor(&left, 0, true);
+  turn_motor(&right, 0, true);
+  delay(50);
 }
+
